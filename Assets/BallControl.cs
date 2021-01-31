4e02b15e356ac38791d182e3a455d4cb22d18c2b
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-01-31 07:45:10 +0700

    create pong game with trajectory ball

---
+++ Assets/BallControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+    public float pushForce;
+    // Titik asal lintasan Bola
+    private Vector2 trajectoryOrigin;
+    void Start()
+    {
+        trajectoryOrigin = transform.position;
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        RestartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ResetBall()
+    {
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+   void PushBall()
+    {
+        // Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
+        float yRandomInitialForce = pushForce;
+
+        // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+        float randomDirection = Random.Range(0, 2);
+
+        // Jika nilainya di bawah 1, bola bergerak ke kiri. 
+        // Jika tidak, bola bergerak ke kanan.
+        if (randomDirection < 1.0f)
+        {
+            // Gunakan gaya untuk menggerakkan bola ini.
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+
+    void RestartGame() {
+        ResetBall();
+        Invoke("PushBall", 2); // jalankan PushBall() setelah 2s
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        trajectoryOrigin = transform.position;
+    }
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Tombol untuk menggerakkan ke atas
+    public KeyCode upButton = KeyCode.W;
+ 
+    // Tombol untuk menggerakkan ke bawah
+    public KeyCode downButton = KeyCode.S;
+ 
+    // Kecepatan gerak
+    public float speed = 10.0f;
+ 
+    // Batas atas dan bawah game scene (Batas bawah menggunakan minus (-))
+    public float yBoundary = 9.0f;
+ 
+    // Rigidbody 2D raket ini
+    private Rigidbody2D rigidBody2D;
+ 
+    // Skor pemain
+    private int score;
+    // Titik tumbukan terakhir with ball
+    private ContactPoint2D lastContactPoint;
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move player
+        Vector2 velocity = rigidBody2D.velocity;
+
+        if (Input.GetKey(upButton)) { // ke atas
+            velocity.y = speed;
+        } else if (Input.GetKey(downButton)) { // ke bawah
+            velocity.y = -speed;
+        } else {
+            velocity.y = 0.0f;
+        }
+
+        rigidBody2D.velocity = velocity;
+
+
+        // boundary
+        Vector3 position = transform.position;
+
+        if (position.y > yBoundary) {
+            position.y = yBoundary;
+        } else if (position.y < -yBoundary) {
+            position.y = -yBoundary;
+        }
+
+        transform.position = position;
+    }
+
+    public void IncrementScore() {
+        score++;
+    }
+
+    public void ResetScore() {
+        score = 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.name.Equals("Ball")) {
+            lastContactPoint = other.GetContact(0);
+        }
+    }
+
+    public int Score {
+        get {
+            return score;
+        }
+    }
+
+    public ContactPoint2D LastContactPoint {
+        get {
+            return lastContactPoint;
+        }
+    }
+}
